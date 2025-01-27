94daa531edeeb3b2848d05bc8c7b6a2c738cd012
nonbots <nhantdbui@gmail.com>
2025-01-27 10:44:52 -0500

    added more LS problems

---
+++ objects/simplifyObjectTwoProperties.js
@@ -0,0 +1,21 @@
+/*
+You were tasked with making a list of every makeup item your local pharmacy had in stock. You created a very long array of each item, with each element having both a name and brand property. Now you're looking to simplify the list by combining duplicate items, and adding a count property to everything.
+
+Example
+simplifyList([
+  { brand: "NARS", name: "Cosmetics Voyageur Pallete" },
+  { brand: "NARS", name: "Cosmetics Voyageur Pallete" },
+  { brand: "Urban Decay", name: "Naked Honey Pallete" },
+  { brand: "Stila", name: "Stay All Day Liquid Lipstick" },
+  { brand: "Stila", name: "Stay All Day Liquid Lipstick" },
+  { brand: "Stila", name: "Stay All Day Liquid Lipstick" }
+]) ➞ [
+  { brand: "NARS", name: "Cosmetics Voyageur Pallete", count: 2 },
+  { brand: "Urban Decay", name: "Naked Honey Pallete", count: 1 },
+  { brand: "Stila", name: "Stay All Day Liquid Lipstick", count: 3 }
+]
+Notes
+There will always be one or more element in the given array.
+Each element will always have a brand and name property.
+All duplicates will be displayed in order.
+  */
